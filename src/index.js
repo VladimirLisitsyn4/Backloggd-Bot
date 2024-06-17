a3ac9df3d7898906eba1a855112eab7553808d14
Vlad Lisitsyn <50640699+VladimirLisitsyn4@users.noreply.github.com>
2024-06-17 17:57:43 -0400

    implemented proper error catching into the function so it still sends whatever info it can find upon an error

--- src/index.js
+++ src/index.js
@@ -30,19 +30,31 @@ client.on('interactionCreate', async (interaction) => {
         interaction.reply("yupyup");
 
     if (interaction.commandName == 'get-info') {
-        await interaction.deferReply();  // Acknowledge the interaction
+        await interaction.deferReply();
 
         try {
             const username = interaction.options.get('username').value;
             const userUrl = `https://www.backloggd.com/u/${username}`;
-            const response = await axios.get(userUrl);
+            let response;
+            try {
+                response = await axios.get(userUrl);
+            } catch (error) {
+                //console.error('Error fetching the username:', error);
+                return interaction.editReply('This user does not exist.');
+            }
             const html = response.data;
             const $ = cheerio.load(html);
 
             const usernameText = $('h3.main-header').text().trim();
             const favGameUrl = `https://www.backloggd.com${$('#profile-favorites').find('.ultimate_fav a.cover-link').attr('href')}`;
             
-            const favGameResponse = await axios.get(favGameUrl);
+            let favGameResponse;
+            try {
+                favGameResponse = await axios.get(favGameUrl);
+            } catch (error) {
+                //console.error('Error fetching the user\'s favorite game:', error);
+                return interaction.editReply(`Found user: ${usernameText}`);
+            }
             const favGameHtml = favGameResponse.data;
             const $$ = cheerio.load(favGameHtml);
 
